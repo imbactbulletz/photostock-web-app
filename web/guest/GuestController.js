1efee6f884280e85677c94a47de302ccd06b7c91
wybz <scvetic16@raf.rs>
2018-08-14 06:00:32 +0200

    Implemented login functionality for frontend.

--- web/guest/GuestController.js
+++ web/guest/GuestController.js
@@ -1,6 +1,6 @@
 var app = angular.module("photostock-app");
 
-app.controller("GuestController", ['$scope', '$location', function ($scope, $location){
+app.controller("GuestController", ['$scope', '$rootScope', '$location', 'GuestService', function ($scope, $rootScope, $location, GuestService){
 
     // navigate through navbar
     $scope.goto = function(page){
@@ -20,21 +20,52 @@ app.controller("GuestController", ['$scope', '$location', function ($scope, $loc
         }
     };
 
-
+    // validates input and tries to log user in
     $scope.login = function(){
-        var user = $scope.user;
-
+        var user = angular.copy($scope.user);
 
         // validates
         if (user === undefined || user.username === undefined || user.password === undefined || user.username == "" || user.password == ""){
             return;
         }
 
+        // sends request to REST API via service
+        GuestService.login(user).then(function(response){
+            var returned_value = response.data;
 
-        $scope.loginError = true;
+            // bad login
+            if(returned_value == null){
+                $scope.loginError = true;
+                return;
+            }
+
+            if(returned_value.account_type === "regular"){
+                alert("You've logged in with a regular account!");
+            }
+
+            else if(returned_value.account_type === "vendor"){
+                alert("You've logged in with a vendor account!");
+            }
+
+            else if(returned_value.account_type === "moderator"){
+                alert("You've logged in with an moderator account!");
+            }
+
+            else if(returned_value.account_type === "administrator"){
+                alert("You've logged in with an administrator account!");
+            }
+
+            else if(returned_value.account_type === "operator"){
+                alert("You've logged in with an operator account!");
+            }
+
+            // saving user to rootScope
+            $rootScope.user = response.data;
+        });
     };
 
 
+    // redirects to a template page
     $scope.goto = function(page){
         if(page == "/forgotPassword"){
             $location.path("/forgotPassword");
--- web/guest/GuestService.js
+++ web/guest/GuestService.js
@@ -1,1 +1,11 @@
 var app = angular.module("photostock-app");
+
+app.factory('GuestService', ['$http', function($http){
+    var service = {};
+
+    service.login = function(user){
+       return $http.post("http://localhost:8080/Photostock/rest/user/login", user);
+    };
+
+    return service;
+}]);
